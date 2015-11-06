1f4540fe83c00416a3222e46f26829f5b97c1ba9
Jorge Cisneros <fraxito@gmail.com>
2015-11-06 19:22:58 +0100

    Finalizada la piramide invertida
    falta todavia girarla

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -15,9 +15,22 @@ public class Ejercicio1 extends acm.program.GraphicsProgram{
 		//llamo al método que dibuja la pirámide
 		pintaPiramide();
 	}
-	
+
 	public void pintaPiramide(){
+		int numLadrillosBase = 14;
+		int anchoLadrillo = 30;
+		int altoLadrillo = 12;
 		
+		//querido yo del martes:
+		//te falta lo mas facil que es girar la piramide
+		// att: tu yo del viernes
+		for (int j=0; j< numLadrillosBase; j++){
+			for (int i=0; i< numLadrillosBase-j; i++){
+				GRect ladrillo = new GRect(anchoLadrillo, altoLadrillo);
+				add(ladrillo,anchoLadrillo*j/2 + anchoLadrillo * i, altoLadrillo*j);
+				pause(20);
+			}
+		}
 	}
 }
 
