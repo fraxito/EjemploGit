9be835f897e49d1eaba83c3b63546c1aceef765c
Jorge Cisneros <fraxito@gmail.com>
2015-11-06 17:10:02 +0100

    Inicio del proyecto
    Solo tiene una clase que pone un mensaje en la pantalla

---
+++ src/Ejercicio1.java
@@ -0,0 +1,16 @@
+/*
+ * Autor: Jorge Cisneros
+ * 
+ * Ejemplo de uso de GitHub
+ */
+
+import acm.graphics.*;
+
+public class Ejercicio1 extends acm.program.GraphicsProgram{
+
+
+	public void run () {
+		GLabel etiqueta = new GLabel("Hola Git!!");
+		add (etiqueta, 120,130);
+	}
+}
